cb2dab5891d35c57af24614cbf72c54238f6a7eb
Hugo Hrbáň <hugohrban2@gmail.com>
2023-08-22 20:30:27 +0200

    docs and cleanup in HiddenFile

--- HiddenFile.cs
+++ HiddenFile.cs
@@ -13,24 +13,7 @@ namespace Steganography
         public static readonly byte[] magicNumber = {0x73, 0x74, 0x65, 0x67,
             0x61, 0x6e, 0x6f, 0x67, 0x72, 0x61, 0x70, 0x68, 0x79};
 
-        //public IStegImage HideInImage(string imgPath, StegType stegType)
-        //{
-        //    if (stegType == StegType.LSbEncoding)
-        //    {
-        //        return HideInImageLSb(imgPath);
-        //    }
-        //    else if (stegType == StegType.JSteg)
-        //    {
-        //        return HideInImageJSteg(imgPath);
-        //    }
-        //    else
-        //    {
-        //        throw new ArgumentException("Invalid encoding scheme");
-        //    }
-        //}
-
         // convert chars in a string to a byte array
-
         private byte[] GetBytes(string s)
         {
             byte[] bytes = new byte[s.Length * 2];
@@ -43,28 +26,32 @@ namespace Steganography
             return bytes;
         }
 
-        /**
-         * Class for preprocessing a file to be hidden in an image. Content of file is read as an array of bytes and prepended with
-         * specific metadata to store the file name, encoding parameters, etc.
-         * HiddenFile data structure:
-         *      bytes 0-12 - magic number
-         *      byte 13 - bitsPerByte - number of least-significant bits changed (1-8)
-         *      byte 14 - length of filename in bytes (max 255 ASCII characters, 1 char = 2 bytes)
-         *      bytes 15-x - filename
-         *      bytes (x+1)-(x+5) - length of file data in bytes (files up to 2^32 B = 2^22 kB = 2^12 MB = 4 GB)
-         *      bytes (x+6)-... - file data
-         * Bytes 0-13 are be encoded using one bit per byte encoding (only change one least significant bit of a byte),
-         * the rest will be hidden using bitsPerByte encoding, as specified in the metadata, so that we can decode it after
-         * finding out bitsPerByte. bitsPerByte parameter can be specified by the user.
-         */
+        
+         
+        
+        /// <summary>
+        /// Class for preprocessing a file to be hidden in an image. Content of file is read as an array of bytes and prepended with
+        /// specific metadata to store the file name, encoding parameters, etc.
+        /// HiddenFile data structure:
+        ///      bytes 0-12 - magic number
+        ///      byte 13 - bitsPerByte - number of least-significant bits changed (1-8)
+        ///      byte 14 - length of filename in bytes (max 255 UTF-16 characters, 1 char = 2 bytes)
+        ///      bytes 15-x - filename
+        ///      bytes (x+1)-(x+5) - length of file data in bytes (files up to 2^32 B = 2^22 kB = 2^12 MB = 4 GB)
+        ///      bytes (x+6)-... - file data
+        /// In case of LSB encoding, bytes 0-13 are be encoded using one bit per byte encoding (only change one least significant bit of a byte),
+        /// the rest will be hidden using bitsPerByte encoding, as specified in the metadata, so that we can decode it after
+        /// finding out bitsPerByte. bitsPerByte parameter can be specified by the user.
+        /// In case of jsteg bitsPerByte is irrelevant.
+        /// </summary>
+        /// <param name="path"> Path to the file to be hidden</param>
+        /// <param name="st"> Type of encoding to use. Either jsteg or LSb </param>
+        /// <param name="bitsPerByte"> Number of LS bits to change. Defaults to 1 for jsteg. </param>
         public HiddenFile(string path, StegType st, int bitsPerByte=1)
         {
             this.bitsPerByte = bitsPerByte;
-            if (st == StegType.JSteg)
-            {
-                // TODO
-                this.bitsPerByte = 1;
-            }
+            if (st == StegType.JSteg) this.bitsPerByte = 1;
+            
             fileName = Path.GetFileName(path);
             byte[] fileNameBytes = GetBytes(fileName);
             byte[] fileData = File.ReadAllBytes(path);
