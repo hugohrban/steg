1b9b4aea9453b77469f68307f527de4b8ffdb47e
Hugo Hrbáň <hugohrban2@gmail.com>
2023-08-22 23:17:18 +0200

    better help messages

--- Program.cs
+++ Program.cs
@@ -7,12 +7,14 @@ namespace Steganography
         public static void Main(string[] args)
         {
             string helpMessage = "Usage: steg <command> [<args>]\n" +
-                "The available steganography commands are:\n" +
-                "   hide [METHOD] [FILE] [INPUT_IMG] [OUTPUT_IMG]\n" +
+                "Steganography tool for hiding arbitrary files in images.\n" +
+                "The available commands are:\n" +
+                "   hide [METHOD] [FILE] [INPUT_IMG] [Q/bpB]\n" +
                 "   extract [METHOD] [IMG]\n" +
                 "   capacity [METHOD] [IMG]\n" +
+                "   compress [INPUT_IMG] [OUTPUT_IMG] [QUALITY]\n" +
                 "   help [command]\n" +
-                "Use help [command] for more information about a command\n";
+                "Use help [command] for more information about a command";
     
             if (args.Length < 1)
             {
@@ -77,25 +79,46 @@ namespace Steganography
                     }
                     if (args[1] == "hide")
                     {
-                        System.Console.WriteLine("Usage: steg hide [METHOD] [FILE] [INPUT_IMG] [OUTPUT_IMG]\n" +
-                            "Hide a file in an image\n" +
+                        System.Console.WriteLine("Usage: steg hide [METHOD] [FILE] [INPUT_IMG] [Q/bpB]\n" +
+                            "Hide a file in an image.\n\n" +
                             "METHOD can be either `jsteg` or `lsb`:\n" +
-                            "    `jsteg` - hides the file data in the AC coefficients of the jpeg image\n" +
-                            "    `lsb` - hides the file data in the least significant bits of the pixels of the image\n" +
+                            "    - `jsteg` - hides the file data in the DCT coefficients of the jpeg image.\n" +
+                            "    - `lsb`   - hides the file data in the least significant bits of the pixels of the image\n" +
                             "FILE       - path to the file to be hidden\n" +
                             "INPUT_IMG  - path to the image to hide the file in\n" +
-                            "OUTPUT_IMG - path to the image to save the result to\n");
+                            "Q/bpB      - quality of jpeg compression (1-100) or number of least significant bits to change (1-8) depending on the METHOD\n");
                     }
                     else if (args[1] == "extract")
                     {
                         System.Console.WriteLine("Usage: steg extract [METHOD] [IMG]\n" +
-                            "Extract a file from an image\n" +
-                            "METHOD can be either `jsteg` or `lsb`\n" +
-                            "IMG is the path to the image to extract the hidden file from\n" +
+                            "Extract a file from an image.\n\n" +
+                            "METHOD - either `jsteg` or `lsb`\n" +
+                            "IMG    - the path to the image to extract the hidden file from\n" +
                             "The file will be saved in the current directory as 'extr_' + its original name.");
                     }
-                    else
+                    else if (args[1] == "capacity")
                     {
+                        System.Console.WriteLine("Usage: steg capacity [METHOD] [IMG]\n" +
+                            "Print the capacity of the image (size of file that can be hidden) for various settings of METHOD\n\n" +
+                            "METHOD - either `jsteg` or `lsb`\n" +
+                            "IMG    - the path to the image to determine the capacity of");
+                    }
+                    else if (args[1] == "compress")
+                    {
+                        System.Console.WriteLine("Usage: steg compress [INPUT_IMG] [OUTPUT_IMG] [QUALITY]\n" +
+                            "Compress an image using jpeg compression.\n\n" +
+                            "INPUT_IMG  - path to the image to be compressed\n" +
+                            "OUTPUT_IMG - path to the compressed image\n" +
+                            "QUALITY    - quality of jpeg compression (1-100)");
+                    }
+                    else if (args[1] == "help")
+                    {
+                        System.Console.WriteLine("Usage: steg help [command]\n" +
+                            "Print help message for a command.\n" +
+                            "command - the command to print help for (hide, extract, capacity, compress, help)\n");
+                    } 
+                    else
+                    {             
                         System.Console.WriteLine(helpMessage);
                     }
                     break;
